468db38375bc1dcd04bfcbe1c6beaa992d764970
Nico Ignacio <norbertoignaciojr@gmail.com>
2022-08-24 22:03:06 +0800

    google auth token fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,16 +3,16 @@ import jwt from "jsonwebtoken";
 const verifyAuth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
         const isCustomAuth = token.length < 500;
         let decodedData;
 
         if (token && isCustomAuth) {
             decodedData = jwt.verify(token, "testToken");
-
             req.userId = decodedData?.id;
         } else {
             // GOOGLE TOKEN
-            decodedData = jwt.verify(token);
+            decodedData = jwt.decode(token);
             req.userId = decodedData?.sub;
         }
 
