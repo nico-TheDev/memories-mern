8b58d5877ededcaed77ea6369376960390349abc
Nico Ignacio <norbertoignaciojr@gmail.com>
2022-08-28 21:12:03 +0800

    added pagination

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -2,10 +2,23 @@ import PostMessage from "../models/PostMessage.js";
 import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
+    const { page } = req.query;
 
-        res.status(200).json({ data: postMessages });
+    try {
+        const LIMIT = 6;
+        const startIndex = (Number(page) - 1) * LIMIT; // starting index of every page
+        const total = await PostMessage.countDocuments(); // returns the total number of documents
+
+        const posts = await PostMessage.find()
+            .sort({ _id: -1 })
+            .limit(LIMIT)
+            .skip(startIndex);
+
+        res.status(200).json({
+            data: posts,
+            currentPage: Number(page),
+            numberOfPages: Math.ceil(total / LIMIT),
+        });
     } catch (err) {
         res.status(404).json({
             message: error,
@@ -15,14 +28,13 @@ export const getPosts = async (req, res) => {
 
 export const getPostsBySearch = async (req, res) => {
     const { searchQuery, tags } = req.query;
-    console.log(searchQuery);
-    console.log(tags);
-
     try {
         const title = new RegExp(searchQuery, "i");
+
         const posts = await PostMessage.find({
             $or: [{ title }, { tags: { $in: tags.split(",") } }],
         });
+
         console.log(posts);
         res.status(200).json({ posts });
     } catch (err) {
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken";
 const verifyAuth = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        console.log(token);
         const isCustomAuth = token.length < 500;
         let decodedData;
 
@@ -19,6 +18,7 @@ const verifyAuth = async (req, res, next) => {
         next();
     } catch (err) {
         console.log(err);
+        res.status(404).json({ message: err });
     }
 };
 
